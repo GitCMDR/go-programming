64b74a1ac6d3f5adac6a73b02369e1d82adc5056
Luis Benavides <github@luisbenavides.xyz>
2019-11-07 21:55:12 -0600

    D4 Quick recap on shorthand declarations in #Golang ! #100DaysOfCode

---
+++ 001X1-ShortDeclaration/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42 // this is how you use short declaration
+	fmt.Println(x)
+	x = 99 // you don't have to declare it again - you just assign to it
+	fmt.Println(x)
+}
