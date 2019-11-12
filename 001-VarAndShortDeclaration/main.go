ffab18877cd149a0d2ac0da47e3ebb8ca3f5192e
Luis Benavides <github@luisbenavides.xyz>
2019-11-11 21:31:40 -0600

    D5 Variable scoping, var, and more on shorthand declarations in #Golang ! #100DaysOfCode

---
+++ 001-VarAndShortDeclaration/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// DECLARE there is a Variable with the Identifier "z" and
+// that variable is of type int
+var z int
+
+func main() {
+	// Short Declaration Operator
+	// Then Declare A Value and Assign a Value
+
+	x := 42
+	fmt.Println(x)
+
+	var y = 43
+	fmt.Println(y)
+
+	fmt.Println(z)
+}
